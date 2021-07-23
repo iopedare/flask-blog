707e9295806c6ef4f4e3bebeabf00ef30384f0cb
Dayo Opedare <djetlii@live.com>
2021-07-23 21:28:59 +0100

    user logins

--- config.py
+++ config.py
@@ -7,5 +7,4 @@ load_dotenv(os.path.join(basedir, ".env"))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-    SQLALCHEMY_TRACK_NOTIFICATIONS = False
-    
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
