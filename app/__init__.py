e0261b40fe9d6d5c82c6d3d8789cc06383ed2351
Dayo Opedare <djetlii@live.com>
2021-07-27 16:36:52 +0100

    email support

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,23 @@
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
+import smtplib
 import os
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
+
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+mail = Mail(app)
+
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
@@ -26,18 +31,13 @@ if not app.debug:
             mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
             fromaddr='no-reply@' + app.config['MAIL_SERVER'],
             toaddrs=app.config['ADMINS'], subject='Ioped Blog Failure',
-            credentials=auth, secure=secure
-        )
+            credentials=auth, secure=secure)
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
 
     if not os.path.exists('logs'):
         os.mkdir('logs')
-    file_handler = RotatingFileHandler(
-        'logs/microblog.log',
-        maxBytes=10240,
-        backupCount=10
-    )
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240, backupCount=10)
     file_handler.setFormatter(logging.Formatter(
             '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
     file_handler.setLevel(logging.INFO)
